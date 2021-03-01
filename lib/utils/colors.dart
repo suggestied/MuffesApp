5423755965fe0c5713d502da66a1f0feadfa347f
suggestied <74950245+suggestied@users.noreply.github.com>
2021-03-01 16:28:18 -0400

    Changed colors (icy)

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 
 class customStyle {
-  final primaryColor = Color.fromRGBO(111, 100, 237, 1);
+  final primaryColor = Color.fromRGBO(130, 166, 237, 1);
   final secondaryColor = Color.fromRGBO(246, 246, 246, 1);
   final disabledColor = Color.fromRGBO(181, 181, 181, 1);
   final darkGrayColor = Color.fromRGBO(22, 22, 22, 1);
   final lightColor = Color.fromRGBO(250, 250, 250, 1);
-  final blueColor = Color.fromRGBO(133, 123, 189, 1);
+  final blueColor = Color.fromRGBO(121, 132, 219, 1);
   final kindaLightColor = Color.fromRGBO(232, 232, 232, 1);
 
   final Shader bluePurpleGradient = LinearGradient(
-    colors: [Color.fromRGBO(111, 100, 237, 1), Color.fromRGBO(183, 97, 232, 1)],
+    colors: [
+      Color.fromRGBO(130, 166, 237, 1),
+      Color.fromRGBO(121, 132, 219, 1)
+    ],
   ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
 
   final bluePurpleGradientNotShaders = LinearGradient(colors: [
-    Color.fromRGBO(111, 100, 237, 1),
-    Color.fromRGBO(183, 97, 232, 1)
+    Color.fromRGBO(130, 166, 237, 1),
+    Color.fromRGBO(121, 132, 219, 1)
   ], begin: Alignment.centerLeft, end: Alignment.centerRight);
 
   Map<int, Color> primaryColorMaterial = {
-    50: Color.fromRGBO(133, 123, 189, .1),
-    100: Color.fromRGBO(133, 123, 189, .2),
-    200: Color.fromRGBO(133, 123, 189, .3),
-    300: Color.fromRGBO(133, 123, 189, .4),
-    400: Color.fromRGBO(133, 123, 189, .5),
-    500: Color.fromRGBO(133, 123, 189, .6),
-    600: Color.fromRGBO(133, 123, 189, .7),
-    700: Color.fromRGBO(133, 123, 189, .8),
-    800: Color.fromRGBO(133, 123, 189, .9),
-    900: Color.fromRGBO(133, 123, 189, 1),
+    50: Color.fromRGBO(121, 132, 219, .1),
+    100: Color.fromRGBO(121, 132, 219, .2),
+    200: Color.fromRGBO(121, 132, 219, .3),
+    300: Color.fromRGBO(121, 132, 219, .4),
+    400: Color.fromRGBO(121, 132, 219, .5),
+    500: Color.fromRGBO(121, 132, 219, .6),
+    600: Color.fromRGBO(121, 132, 219, .7),
+    700: Color.fromRGBO(121, 132, 219, .8),
+    800: Color.fromRGBO(121, 132, 219, .9),
+    900: Color.fromRGBO(121, 132, 219, 1),
   };
 }
 
